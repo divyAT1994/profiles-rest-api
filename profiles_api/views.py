9206c78e511735da0822734021103077ac413a8a
divyAT1994 <uccdivya1994@gmail.com>
2021-06-12 22:21:53 +0530

    Adding filters

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
 
 from profiles_api import serializers
 from profiles_api import models
@@ -116,3 +118,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',) #adding serach filtering to email and name fields
